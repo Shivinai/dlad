Fixed anomaly injection order error, causing incorrect MSE calculation results

AnomalyDetector.py
@@ -3,31 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Autoencoder import SpectrogramAE
 from DataLoader import build_spectrogram
+from scipy.signal import medfilt
 from Macros import generate_frame
 
-def get_threshold(sr, nps, device, snr_db=20, num_calibration_frames=200):
-
+def get_threshold(model, sr, nps, device, snr, num_calibration_frames=1000):
     model.eval()
     all_mse_values = []
-        
+
     with torch.no_grad():
         for _ in range(num_calibration_frames):
-            frame = generate_frame(bpf=BITS_PER_FRAME, snr_limit=SNR_DB)
-            
+            frame = generate_frame(bpf=16, snr=snr)
             spec, _, _ = build_spectrogram(frame, sr, nps)
             spec_tensor = torch.tensor(spec, dtype=torch.float32).unsqueeze(0).unsqueeze(0).to(device)
-            
             recon = model(spec_tensor).squeeze().cpu().numpy()
             
             mse_time = np.mean((spec - recon)**2, axis=0)
             all_mse_values.extend(mse_time)
-            
-    global_mean = np.mean(all_mse_values)
-    global_std = np.std(all_mse_values)
     
-    threshold = global_mean + 4 * global_std
+    all_mse_values = np.array(all_mse_values)
+    threshold = np.mean(all_mse_values) + 4 * np.std(all_mse_values)
     
-    print("Detected threshold: ", threshold)
+    print(f"Detected threshold: {threshold:.6f} (mean: {np.mean(all_mse_values):.6f}, std: {np.std(all_mse_values):.6f})")
     return threshold
 
 def detect_anomaly(model, anomalous_signal, sr, nps, device, threshold):
@@ -40,11 +36,10 @@ def detect_anomaly(model, anomalous_signal, sr, nps, device, threshold):
         anom_recon = model(anom_tensor).squeeze().cpu().numpy()
 
     anom_mse_time = np.mean((anom_spec - anom_recon)**2, axis=0)
-    # window_size = 5
-    # anom_mse_smoothed = np.convolve(anom_mse_time, np.ones(window_size)/window_size, mode='same')
 
-    #anomaly_indices = np.where(anom_mse_smoothed > THRESHOLD)[0]
-    anomaly_indices = np.where(anom_mse_time > threshold)[0]
+    anom_mse_smoothed = medfilt(anom_mse_time, kernel_size=3)
+
+    anomaly_indices = np.where(anom_mse_smoothed > threshold)[0]
 
     time_axis_signal = np.linspace(0, len(anomalous_signal) / sr, len(anomalous_signal))
 
@@ -85,11 +80,11 @@ if __name__ == "__main__":
     SR = 44100
     NPS = 256
     BITS_PER_FRAME = 16
-    SNR_DB = 30
-    DEBUG_THRESHOLD = 0.003
+    SNR = 30
+    DEBUG_THRESHOLD = 0.0045
 
-    #device = torch.device("cuda")
-    device = torch.device("cpu")
+    device = torch.device("cuda")
+    #device = torch.device("cpu")
 
     if device.type == "cuda":
         print(f"Using device: {torch.cuda.get_device_name(0)}")
@@ -105,8 +100,8 @@ if __name__ == "__main__":
         print("Weights file is missing")
         exit()
     
-    threshold = get_threshold(sr=SR, nps=NPS, device=device, snr_db=SNR_DB, num_calibration_frames=50)
     #threshold = DEBUG_THRESHOLD
-    anomalous_frame = generate_frame(bpf=BITS_PER_FRAME, snr_limit=SNR_DB, anomaly='dropout')
+    threshold = get_threshold(model=model, sr=SR, nps=NPS, device=device, snr=SNR, num_calibration_frames=50)
+    anomalous_frame = generate_frame(bpf=BITS_PER_FRAME, snr=SNR, anomaly='echo')
 
     detect_anomaly(model, anomalous_frame, SR, NPS, device, threshold)

ChannelSimulation.py
@@ -2,50 +2,63 @@ import numpy as np
 
 class Channel:
 
-    def __init__(self, snr_db=10.0):
-        self.snr_db = snr_db
+    def __init__(self, snr):
+        self.snr = snr
 
         self._anomaly_handlers = {
             'dropout': self._dropout,
             'echo': self._echo,
             'clipping': self._clipping
         }
+
+    def _get_random_range(self, signal_length):
+        duration = int(signal_length * np.round(np.random.uniform(0.1, 0.2), 1))
+        start_idx = np.random.randint(0, signal_length - duration)
+
+        return start_idx, start_idx + duration
     
     def process(self, signal, anomaly_type=None, **anomaly_kwargs):
         s = signal.copy()
         
+        signal_power = np.mean(np.abs(s)**2)
+        if signal_power > 0:
+            noise_power = signal_power / (10 ** (self.snr / 10.0))
+            noise = np.random.normal(0, np.sqrt(noise_power), len(s))
+            s += noise
+        
         if anomaly_type in self._anomaly_handlers:
             s = self._anomaly_handlers[anomaly_type](s, **anomaly_kwargs)
-        elif anomaly_type is not None:
-            raise ValueError(f"Unknown anomaly: '{anomaly_type}'. Falling back to none")
-
-        signal_power = np.mean(np.abs(s)**2)
         
-        if signal_power > 0:
-            noise_power = signal_power / (10 ** (self.snr_db / 10.0))
-            noise = np.random.normal(0, np.sqrt(noise_power), len(s))
-
-            s += noise
-
-            return s
+        return s
 
     def _dropout(self, signal):
         corrupted = signal.copy()
-        duration = int(len(signal) * np.round(np.random.uniform(0.1, 0.7), 1))
-        if len(signal) > duration:
-            start_idx = np.random.randint(0, len(signal) - duration)
-            corrupted[start_idx : start_idx + duration] = 0.0
+        start, end = self._get_random_range(len(signal))
+        corrupted[start:end] *= np.random.uniform(0.01, 0.1)
         return corrupted
 
     def _echo(self, signal):
-        echo = np.zeros_like(signal)
-        ds = np.random.randint(50, 2000)
-        print(f"Using {ds} delay samples")
-        if len(signal) > ds:
-            echo[ds:] = signal[:-ds] * np.round(np.random.uniform(0.3, 0.8), 1)
-        return signal + echo
+        corrupted = signal.copy()
+        start, end = self._get_random_range(len(signal))
+        
+        ds = np.random.randint(50, 1000)
+        gain = np.round(np.random.uniform(0.4, 0.7), 1)
+        
+        segment = signal[start:end].copy()
+        echo_segment = np.zeros_like(segment)
+        
+        if len(segment) > ds:
+            echo_segment[ds:] = segment[:-ds] * gain
+            corrupted[start:end] = segment + echo_segment
+            
+        return corrupted
 
     def _clipping(self, signal):
-        threshold = np.round(np.random.uniform(0.1, 0.6), 1)
-        return np.clip(signal, -threshold, threshold)
+        corrupted = signal.copy()
+        start, end = self._get_random_range(len(signal))
+        
+        threshold = np.round(np.random.uniform(0.1, 0.4), 1)
+        
+        corrupted[start:end] = np.clip(corrupted[start:end], -threshold, threshold)
+        return corrupted
         

Macros.py
@@ -2,17 +2,14 @@ import numpy as np
 from ChannelSimulation import Channel
 from Modem import Modem
 
-np.random.seed(69)
-
-def generate_training_data(nts, bpf, snr_limit):
+def generate_training_data(nts, bpf, snr):
     print("Generating training data")
     modem = Modem(fs=44100, f0=2000, f1=8000, symbol_duration=0.02)
-    channel = Channel(snr_db=snr_limit)
+    channel = Channel(snr=snr)
     training_signals = []
 
     for _ in range(nts):
         source_signal = np.random.randint(0, 2, bpf)
-        snr = np.random.randint(20, snr_limit)
         channel.snr_db = snr
 
         modulated_signal = modem.modulate(source_signal)
@@ -22,9 +19,9 @@ def generate_training_data(nts, bpf, snr_limit):
 
     return training_signals
 
-def generate_frame(bpf, snr_limit, anomaly=None):
+def generate_frame(bpf, snr, anomaly=None):
     modem = Modem(fs=44100 ,f0=2000, f1=8000, symbol_duration=0.02)
-    channel = Channel(snr_db=np.random.randint(20, snr_limit))
+    channel = Channel(snr=snr)
 
     source_signal = np.random.randint(0, 2, bpf)
     modulated_signal = modem.modulate(source_signal)